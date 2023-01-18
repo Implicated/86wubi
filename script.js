27058ecfc014a5fd6cea2382f82238c74063a327
Implicated <implicated000@gmail.com>
2023-01-18 15:00:34 +0800

    feat: Customize

--- script.js
+++ script.js
@@ -1,121 +1,174 @@
+// noinspection JSUnusedGlobalSymbols
+
 function WubiInput() {
-	const whitespaceCharacters = [' ', ' ',
-		'\b', '\t', '\n', '\v', '\f', '\r', `\"`, `\'`, `\\`,
-		'\u0008', '\u0009', '\u000A', '\u000B', '\u000C',
-		'\u000D', '\u0020', '\u0022', '\u0027', '\u005C',
-		'\u00A0', '\u2028', '\u2029', '\uFEFF'];
+    const whitespaceCharacters = [' ', ' ',
+        '\b', '\t', '\n', '\v', '\f', '\r', `\"`, `\'`, `\\`,
+        '\u0008', '\u0009', '\u000A', '\u000B', '\u000C',
+        '\u000D', '\u0020', '\u0022', '\u0027', '\u005C',
+        '\u00A0', '\u2028', '\u2029', '\uFEFF'];
 
-	const stop_words = '\'"()[]{}<>.,:;!?*&^%$#@~`-_=+/\\|' +
-		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-		'0123456789' +
-		'，。“”《》——！？：、‘’';
+    const stop_words = '\'"()[]{}<>.,:;!?*&^%$#@~`-_=+/\\|' +
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        '0123456789' +
+        '，。“”《》——！？：、‘’';
 
-	function isSpace(str) {
-		for (const space of whitespaceCharacters) {
-			if (str.indexOf(space) > -1) {
-				return true;
-			}
-		}
-		return false;
-	}
+    function isSpace(str) {
+        for (const space of whitespaceCharacters) {
+            if (str.indexOf(space) > -1) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	function isStopWord(str) {
-		return stop_words.indexOf(str) > -1;
-	}
+    function isStopWord(str) {
+        return stop_words.indexOf(str) > -1;
+    }
 
-	function moveInputBox() {
-		const cursor = document.getElementById('cursor');
-		const {left, top} = cursor.getBoundingClientRect();
-		const {offsetLeft, offsetTop, offsetHeight} = cursor;
-		const inputbox = document.getElementById('inputbox');
-		inputbox.style.top = offsetTop + offsetHeight;
-		inputbox.style.left = offsetLeft;
-		const inputtext = document.getElementById('inputtext');
-		inputtext.focus();
-	}
+    function moveInputBox() {
+        const cursor = document.getElementById('cursor');
+        const {offsetLeft, offsetTop, offsetHeight} = cursor;
+        const inputBox = document.getElementById('inputBox');
+        inputBox.style.top = offsetTop + offsetHeight;
+        inputBox.style.left = offsetLeft + 15;
+        const inputText = document.getElementById('inputText');
+        inputText.focus();
+    }
 
-	return {
-		target: "Loading...",
-		progress: Number(localStorage.getItem("progress")) || 0,
-		inputtext: '',
+    return {
+        target: "Loading...",
+        progress: Number(localStorage.getItem("progress")) || 0,
+        letter: Number(localStorage.getItem("letter")) || 0,
+        inputText: '',
 
-		showTextarea: false,
-		isStopWord,
+        showTextarea: false,
+        showInputBox: true,
+        isStopWord,
 
-		getText() {
-			let target = localStorage.getItem("target");
-			if (target) {
-				this.target = target;
-				setTimeout(() => moveInputBox(), 0);
-				return;
-			} else {
-				fetch('text.txt')
-					.then(response => response.text())
-					.then(text => {
-						this.target = text;
-						setTimeout(() => moveInputBox(), 0);
-					});
-			}
-		},
+        getText() {
+            let target = localStorage.getItem("target");
+            if (target) {
+                this.target = target;
+                setTimeout(() => moveInputBox(), 0);
+            } else {
+                fetch('text/common.txt')
+                    .then(response => response.text())
+                    .then(text => {
+                        this.target = text.trim();
+                        setTimeout(() => moveInputBox(), 0);
+                    });
+            }
+        },
 
-		getTargetDone() {
-			let {target, progress} = this;
-			return target.substring(0, progress);
-		},
+        refreshText(file) {
+            fetch(file)
+                .then(response => response.text())
+                .then(text => {
+                    this.target = text.trim();
+                    setTimeout(() => moveInputBox(), 0);
+                    localStorage.setItem("progress", '0');
+                    localStorage.setItem("letter", '0');
+                    localStorage.setItem("target", text);
+                    location.reload()
+                });
+        },
 
-		getTargetTodo() {
-			let {target, progress} = this;
-			return target.substring(progress);
-		},
+        randomText() {
+            let target = localStorage.getItem("target");
+            if (target) {
+                const text = shuffle(Array.from(target)).join("").trim()
+                this.target = text;
+                setTimeout(() => moveInputBox(), 0);
+                localStorage.setItem("progress", '0');
+                localStorage.setItem("letter", '0');
+                localStorage.setItem("target", text);
+                location.reload()
+            }
+        },
 
-		getCharacter() {
-			return this.target[this.progress];
-		},
+        getTargetDone() {
+            let {target, progress} = this;
+            return target.substring(0, progress);
+        },
 
-		onTextChanged() {
-			let {target, progress, inputtext} = this;
+        getTargetTodo() {
+            let {target, progress} = this;
+            return target.substring(progress);
+        },
 
-			if (progress >= target.length) {
-				return;
-			}
+        getCharacter() {
+            return this.target[this.progress];
+        },
 
-			let hasEnter = false;
-			let hasSpace = false;
-			let newInputtext = '';
-			for (let i = 0; i < inputtext.length; i++) {
-				const ch = inputtext[i];
-				if (target[progress] === ch) {
-					progress += 1;
-				} else {
-					newInputtext += ch;
-				}
-				if ('\n' === ch) {
-					hasEnter = true;
-				}
-				if (isSpace(ch)) {
-					hasSpace = true;
-				}
-			}
+        onTextChanged() {
+            let {target, progress, letter, inputText} = this;
+            if (!inputText.match(/[\u4e00-\u9fa5]+/g)) {
+                return;
+            }
 
-			while (progress < target.length && isSpace(target[progress])) {
-				progress += 1;
-			}
+            let hasEnter = false;
+            let hasSpace = false;
+            let newInputText = '';
+            for (let i = 0; i < inputText.length; i++) {
+                const ch = inputText[i];
+                if (target[progress] === ch) {
+                    progress += 1;
+                    letter += 1;
+                } else {
+                    newInputText += ch;
+                }
+                if ('\n' === ch) {
+                    hasEnter = true;
+                }
+                if (isSpace(ch)) {
+                    hasSpace = true;
+                }
+            }
 
-			if (hasEnter) {
-				newInputtext = '';
-			}
-			this.progress = progress;
-			this.inputtext = newInputtext;
-			moveInputBox();
+            while (progress < target.length && isSpace(target[progress])) {
+                progress += 1;
+            }
 
-			localStorage.setItem("progress", progress);
-			localStorage.setItem("target", target);
-		},
+            if (hasEnter) {
+                newInputText = '';
+            }
+            this.progress = progress;
+            this.letter = letter;
+            this.inputText = newInputText;
+            moveInputBox();
 
-		resetProgress() {
-			this.progress = 0;
-			this.onTextChanged();
-		},
-	};
+            if (letter >= target.replace(/\s+/g, '').trim().length) {
+                this.progress += 1;
+                this.getTargetDone()
+                this.showInputBox = false;
+                if (confirm("重置进度") === true) {
+                    this.resetProgress()
+                }
+            }
+            localStorage.setItem("progress", progress);
+            localStorage.setItem("letter", letter);
+            localStorage.setItem("target", target);
+        },
+
+        resetProgress() {
+            this.progress = 0;
+            this.letter = 0;
+            this.showInputBox = true;
+            this.onTextChanged();
+        },
+    };
 }
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+}
+
+function error(obj) {
+    obj.src = "cache/error.gif";
+}
